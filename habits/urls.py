cd9ab69414c8e4d9a79f168b207a62feb5628f52
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2024-06-30 15:28:59 +0300

    Исправление

--- habits/urls.py
+++ habits/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 
 from habits.apps import HabitsConfig
 from habits.views import HabitListApiView, HabitRetrieveApiView, HabitCreateApiView, HabitUpdateApiView, \
-    HabitDestroyApiView, PublicHabitListApiView
-
+    HabitDestroyApiView, PublicHabitListApiView, OwnerHabitListAPIView
 
 app_name = HabitsConfig.name
 
@@ -13,6 +12,6 @@ urlpatterns = [
     path('habits/<int:pk>/', HabitRetrieveApiView.as_view(), name='habits_detail'),
     path('habits/update/<int:pk>/', HabitUpdateApiView.as_view(), name='habits_update'),
     path('habits/delete/<int:pk>/', HabitDestroyApiView.as_view(), name='habits_delete'),
-
+    path('habit/list/owner/', OwnerHabitListAPIView.as_view(), name='owner_habits-list'),
     path('public_habits/', PublicHabitListApiView.as_view(), name='public_habits_list'),
 ]
