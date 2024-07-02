f54b65dfd6c4898a74d0f1a07e8b8091a7bdfb0a
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2024-07-02 11:25:27 +0300

    Исправление по курсовой №7

--- habits/services.py
+++ habits/services.py
@@ -20,10 +20,7 @@ def send_telegram_message(message, chat_id):
 
     url = f'https://api.telegram.org/bot{token}/sendMessage'
     response = requests.post(url, data=data)
-    if response.status_code == 200:
-        return response
-    else:
-        return False
+    response.raise_for_status()
 
 
 def habit_scheduler():
